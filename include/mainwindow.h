49440785140eb67c6b065dd6d0dbca7727a6a6a5
choushunn <zhaung25712@163.com>
2023-05-17 14:06:53 +0800

    include

---
+++ include/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+
+#include "appinit.h"
+#include "appevent.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    Ui::MainWindow *ui;
+
+private slots:
+    void on_m_btn_open_camera_clicked(bool checked);
+    void showFrame(cv::Mat frame);
+
+    void on_pushButton_clicked(bool checked);
+
+private:
+
+    AppInit*    appInit;
+    AppEvent*   appEvent;
+    QThread*    appThread;
+    QTimer*     m_timer = nullptr;
+    int         fps = 30;
+
+};
+#endif // MAINWINDOW_H
